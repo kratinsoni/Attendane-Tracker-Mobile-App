93e1891535b01d9e6cfda74505104036cd48db08
Harshit Soni <kratin67soni@gmail.com>
2026-02-02 22:52:42 +0530

    chore: update dependencies and add Tailwind CSS configuration
    - Added nativewind and prettier-plugin-tailwindcss to package.json
    - Updated react-native-reanimated and react-native-safe-area-context versions
    - Added tailwind.config.js for Nativewind configuration
    - Updated tsconfig.json to include nativewind-env.d.ts and metro.config.js

--- app/modal.tsx
+++ app/modal.tsx
@@ -1,29 +1,10 @@
-import { Link } from 'expo-router';
-import { StyleSheet } from 'react-native';
-
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function ModalScreen() {
   return (
-    <ThemedView style={styles.container}>
-      <ThemedText type="title">This is a modal</ThemedText>
-      <Link href="/" dismissTo style={styles.link}>
-        <ThemedText type="link">Go to home screen</ThemedText>
-      </Link>
-    </ThemedView>
+    <SafeAreaView className="flex-1 justify-center items-center bg-white">
+      <Text className="text-lg">Close Modal</Text>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-});
